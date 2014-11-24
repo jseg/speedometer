9628db1f6d0d43b4d4afa2db6739365eb9f36c94
Archil Imnadze <archil@imnadze.ge>
2014-11-24 14:32:11 +0000

    add page icons

--- run/fns/echo_html.php
+++ run/fns/echo_html.php
@@ -9,6 +9,8 @@ function echo_html ($html, $head, $body) {
                 .'<meta http-equiv="Content-Type"'
                 .' content="text/html; charset=UTF-8" />'
                 .'<meta name="viewport" content="width=device-width, user-scalable=no" />'
+                .'<link rel="icon" type="image/png" href="../images/icons/16.png" />'
+                .'<link rel="icon" type="image/png" href="../images/icons/32.png" sizes="32x32" />'
                 .$head
             .'</head>'
             ."<body>$body</body>"
