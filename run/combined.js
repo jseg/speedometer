9b345baca7149d4f096f0d4ce25d9078feee72f3
Archil Imnadze <archil@imnadze.ge>
2014-12-03 08:08:38 +0000

    calculate user position according to last three positions

--- run/combined.js
+++ run/combined.js
@@ -948,25 +948,52 @@ function TextNode (text) {
 ;
 function TripDistance () {
 
-    var prevPosition = null
+    function getAveragePosition () {
+
+        var averageCoords = {
+            latitude: 0,
+            longitude: 0,
+        }
+        prevPositions.forEach(function (prevPosition) {
+            averageCoords.latitude += prevPosition.coords.latitude
+            averageCoords.longitude += prevPosition.coords.longitude
+        })
+        averageCoords.latitude /= prevPositions.length
+        averageCoords.longitude /= prevPositions.length
+
+        return {
+            coords: averageCoords,
+        }
+
+    }
+
     var value = 0
+    var prevPositions = []
 
     return {
-        add: function (position) {
+        add: function (newPosition) {
+
+            var prevPosition
+            if (prevPositions.length) prevPosition = getAveragePosition()
+
+            prevPositions.push(newPosition)
+            if (prevPositions.length > 3) prevPositions.shift()
+
             if (prevPosition) {
+                var position = getAveragePosition()
                 value += DistanceBetweenPositions(prevPosition, position)
             }
-            prevPosition = position
+
         },
         get: function () {
             return value
         },
         reset: function () {
-            prevPosition = null
+            prevPositions.splice(0)
             value = 0
         },
         start: function () {
-            prevPosition = null
+            prevPositions.splice(0)
         },
     }
 
