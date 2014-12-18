f7439dd6e7a7eee81e7edc6fb85d4cf518a133dc
Archil Imnadze <archil@imnadze.ge>
2014-12-18 11:59:56 +0400

    100px compass

--- run/js/CompassPanel.js
+++ run/js/CompassPanel.js
@@ -1,15 +1,39 @@
 function CompassPanel () {
 
     function render () {
+
         c.clearRect(0, 0, size, size)
         c.save()
+
         c.translate(halfSize, halfSize)
-        c.beginPath()
-        c.arc(0, 0, halfSize, 0, Math.PI * 2)
-        c.lineWidth = size / 20
         c.strokeStyle = '#fff'
-        c.stroke()
+
+        var radius = -halfSize * 0.92
+        c.save()
+        c.font = 'bold 22px FreeMono, monospace'
+        c.textAlign = 'center'
+        c.textBaseline = 'top'
+        c.fillStyle = 'red'
+        c.fillText('N', 0, radius)
+        c.fillStyle = '#999'
+        c.rotate(Math.PI / 2)
+        c.fillText('E', 0, radius)
+        c.rotate(Math.PI / 2)
+        c.fillText('S', 0, radius)
+        c.rotate(Math.PI / 2)
+        c.fillText('W', 0, radius)
         c.restore()
+
+        for (var i = 0; i < 16; i++) {
+            c.rotate(Math.PI / 8)
+            c.beginPath()
+            c.moveTo(0, halfSize * 1)
+            c.lineTo(0, halfSize * 0.95)
+            c.stroke()
+        }
+
+        c.restore()
+
     }
 
     var canvas = document.createElement('canvas')
@@ -23,7 +47,7 @@ function CompassPanel () {
     return {
         element: canvas,
         resize: function (scale) {
-            size = 50 * scale * devicePixelRatio
+            size = 100 * scale * devicePixelRatio
             halfSize = size / 2
             canvas.width = canvas.height = size
             render()
