2cce8bb57c872e69b85a319b8224ca7f0b71058a
Archil Imnadze <archil@imnadze.ge>
2014-12-25 09:57:31 +0000

    disable install button if not available

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-var installButton = document.getElementById('installButton')
-installButton.addEventListener('click', function () {
-    var manifest = location.protocol + '/' + location.host + location.pathname + 'webapp-manifest/'
-    var installRequest = navigator.mozApps.install(manifest)
-})
+var mozApps = navigator.mozApps
+if (mozApps) {
+    var installButton = document.getElementById('installButton')
+    installButton.disabled = false
+    installButton.addEventListener('click', function () {
+
+        function restore () {
+            installButton.innerHTML = 'Install'
+        }
+
+        installButton.innerHTML = 'Installing...'
+
+        var manifest = location.protocol + '/' + location.host + location.pathname + 'webapp-manifest/'
+        var installRequest = mozApps.install(manifest)
+        installRequest.onsuccess = installRequest.onerror = function () {
+            clearTimeout(timeout)
+            restore()
+        }
+
+        var timeout = setTimeout(function () {
+            installRequest.onsuccess = installRequest.onerror = null
+            restore()
+        }, 2000)
+
+    })
+}
